e145f58b6024b6539222462ca51bd42832a1b8de
rida el-mazary <elmazary.rida@gmail.com>
2024-04-23 10:18:39 +0100

    Fix : l build

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,15 +8,13 @@ function Header() {
   const { scroll } = useRecoilValue(windowState);
   return (
     <div
-      className={`duration-600 fixed z-50 bg-white/60 flex h-20 w-full items-center justify-between border-b transition-all ease-in-out ${scroll > 200 ? "!bg-white h-[70px] transition-all duration-300 border-b border-primaryColor" : ""} `}
+      className={`duration-600 fixed z-50 flex h-20 w-full items-center justify-between border-b bg-white/60 transition-all ease-in-out ${scroll > 200 ? "h-[70px] border-b border-primaryColor !bg-white transition-all duration-300" : ""} `}
     >
       <div className="container flex w-full items-center justify-between gap-4">
         <Logo />
         <Nav />
         <div className="Header-nav-CallToAction">
-          <Button onClick={() => router.push("#contact")}>
-            Contactez-Nous
-          </Button>
+          <Button>Contactez-Nous</Button>
         </div>
       </div>
     </div>
